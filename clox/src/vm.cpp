c6ae2c90f3ebfa801c7dbc511f03a3b8efd6044a
Apoorva Parajuli <apoorvaparajuli0@gmail.com>
2026-04-17 13:53:26 -0700

    finishing up ch 15

--- clox/src/vm.cpp
+++ clox/src/vm.cpp
@@ -22,7 +22,7 @@ void push(Value value) {
   vm.stackTop++;
 }
 
-void pop() {
+Value pop() {
   vm.stackTop--;
   return *vm.stackTop;
 }
@@ -30,6 +30,12 @@ void pop() {
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
   #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+  #define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while(false)
 
   for(;;) {
     #ifdef DEBUG_TRACE_EXECUTION
@@ -49,6 +55,11 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD: BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:  BINARY_OP(/); break;
+      case OP_NEGATE: push(-pop()); break;
       case OP_RETURN: {
         printValue(pop());
         printf("\n");
@@ -59,6 +70,7 @@ static InterpretResult run() {
 
   #undef READ_BYTE
   #undef READ_CONSTANT
+  #undef BINARY_OP
 }
 
 
